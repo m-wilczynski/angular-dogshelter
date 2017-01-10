5995b899030a3b0a069bd82cacedda2acad5e1bf
Michał Wilczyński <michal.e.wilczynski@gmail.com>
2017-01-10 21:00:02 +0100

    Basic DAL for CRUD operations.

---
+++ src/TIN.Angular/Controllers/DogController.cs
@@ -0,0 +1,17 @@
+﻿namespace TIN.Angular.Controllers
+{
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+    
+    public class DogController : Controller
+    {
+        private readonly DogContext _context;
+
+        public DogController(DogContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
---
+++ src/TIN.Angular/Migrations/DogContextModelSnapshot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TIN.Angular.Data;
+
+namespace TIN.Angular.Migrations
+{
+    [DbContext(typeof(DogContext))]
+    partial class DogContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.2");
+
+            modelBuilder.Entity("TIN.Angular.Model.Dog", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<bool>("Adopted");
+
+                    b.Property<DateTime?>("AdoptedOn");
+
+                    b.Property<uint>("Age");
+
+                    b.Property<DateTime>("BroughtToShelter");
+
+                    b.Property<string>("CareTaker");
+
+                    b.Property<string>("Description");
+
+                    b.Property<bool>("Male");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Dogs");
+                });
+        }
+    }
+}
