70de3021596b91c10987d856c14e22410379b6b7
Michał Wilczyński <michal.e.wilczynski@gmail.com>
2017-01-10 22:02:20 +0100

    Basic CRUD REST actions for Angular use.

--- src/TIN.Angular/Controllers/DogController.cs
+++ src/TIN.Angular/Controllers/DogController.cs
@@ -1,8 +1,15 @@
 ﻿namespace TIN.Angular.Controllers
 {
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
     using Data;
     using Microsoft.AspNetCore.Mvc;
-    
+    using Microsoft.EntityFrameworkCore;
+    using Model;
+    using Results;
+
+    [Route("api/[controller]")]
     public class DogController : Controller
     {
         private readonly DogContext _context;
@@ -12,6 +19,46 @@
             _context = context;
         }
 
+        public async Task<IActionResult> Index()
+        {
+            return new JsonResult(await _context.Dogs.ToListAsync());
+        }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDogById(int id)
+        {
+            return new JsonResult(await _context.Dogs.SingleOrDefaultAsync(dog => dog.Id == id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddNewDog(Dog dog)
+        {
+            _context.Dogs.Add(dog);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new AjaxResult(false, "Failed to add new dog."));
+            }
+            return new JsonResult(new AjaxResult(true, "New dog added successfully."));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateExistingDog(Dog dog)
+        {
+            var attached = _context.Dogs.Attach(dog);
+            _context.Entry(attached).Entity.State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new AjaxResult(false, "Failed to update dog data."));
+            }
+            return new JsonResult(new AjaxResult(true, "Dog data updated successfully."));
+        }
     }
 }
---
+++ src/TIN.Angular/Results/AjaxResult.cs
@@ -0,0 +1,14 @@
+﻿namespace TIN.Angular.Results
+{
+    public class AjaxResult
+    {
+        public readonly bool Success;
+        public readonly string Message;
+
+        public AjaxResult(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+    }
+}
