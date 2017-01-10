0f06ae222af6f5074cd59230bd5be260a45dd616
Michał Wilczyński <michal.e.wilczynski@gmail.com>
2017-01-10 23:58:48 +0100

    Basic view for dogs list.

---
+++ src/DogShelter/wwwroot/app/dogs-list-cmp.js
@@ -0,0 +1,53 @@
+﻿"use strict";
+
+angular.module("dogShelterApp").component("dogsList",
+{
+    template:
+        "<div class='container'>" +
+        "<table class='table'>" +
+            "<thead>" +
+                "<tr>" +
+                    "<th>Name</th>" +
+                    "<th>Age</th>" +
+                    "<th>Brought to shelter on</th>" +
+                    "<th>Caretaker</th>" +
+                    "<th>Adopted?</th>" +
+                "</tr>" +
+            "</thead>" +
+            "<tbody>" +
+                "<tr ng-repeat='dog in $ctrl.dogs'>" +
+                    "<td>{{dog.name}}</td>" +
+                    "<td>{{dog.age}}</td>" +
+                    "<td>{{dog.broughtToShelter}}</td>" +
+                    "<td>{{dog.careTaker}}</td>" +
+                    "<td>{{dog.adopted}}</td>" +
+                "</tr>" +
+            "</tbody>" +
+        "</table>" +
+        "</div>",
+    controller: function DogListController() {
+        this.dogs = [
+            {
+                name: "Piksel",
+                age: 10,
+                broughtToShelter: "01-10-2017",
+                careTaker: "Michal",
+                adopted: true
+            },
+            {
+                name: "Zuzia",
+                age: 4,
+                broughtToShelter: "01-10-2017",
+                careTaker: "Michal",
+                adopted: true
+            },
+            {
+                name: "Ruby",
+                age: 7,
+                broughtToShelter: "01-10-2017",
+                careTaker: "Michal",
+                adopted: true
+            }
+        ];
+    }
+});
